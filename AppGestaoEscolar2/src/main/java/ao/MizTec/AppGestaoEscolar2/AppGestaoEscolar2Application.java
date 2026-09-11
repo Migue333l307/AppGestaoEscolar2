b850c94e69d58eda8aea7b0419dca40d49a50ac1
Miguel Francisco <migaspardfranck@gmail.com>
2026-09-12 00:20:44 +0100

    O App foi versionado

---
+++ AppGestaoEscolar2/src/main/java/ao/MizTec/AppGestaoEscolar2/AppGestaoEscolar2Application.java
@@ -0,0 +1,13 @@
+package ao.MizTec.AppGestaoEscolar2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AppGestaoEscolar2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AppGestaoEscolar2Application.class, args);
+	}
+
+}
---
+++ AppGestaoEscolar2/src/test/java/ao/MizTec/AppGestaoEscolar2/AppGestaoEscolar2ApplicationTests.java
@@ -0,0 +1,13 @@
+package ao.MizTec.AppGestaoEscolar2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AppGestaoEscolar2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
