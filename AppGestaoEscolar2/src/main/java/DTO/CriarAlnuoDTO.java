49bb9136ba2d1c631d489da590db05d042b1c042
Miguel Francisco <migaspardfranck@gmail.com>
2026-09-12 20:27:31 +0100

    ªCriação da entidade através da @Entity

--- AppGestaoEscolar2/src/main/java/DTO/CriarAlnuoDTO.java
+++ AppGestaoEscolar2/src/main/java/DTO/CriarAlnuoDTO.java
@@ -1,12 +1,19 @@
 package DTO;
 
 import domain.EstadoAluno;
+import jakarta.persistence.*;
 
 import java.time.LocalDate;
 
+//Entity transforma uma classe em entidade do nosso banco de dados;
+
+@Entity
+@Table(name = "aluno")
 public class CriarAlnuoDTO {
 
-
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
     private String numeroMatricula;
     private String nome;
     private String nomeSocial;
